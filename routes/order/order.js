d1f7b47d8d677ffbfd6a188524b8fdebed71c772
robix82 <robertjans82@outlook.com>
2017-11-29 16:04:41 +0100

    added simple order validation

--- routes/order/order.js
+++ routes/order/order.js
@@ -17,12 +17,46 @@ router.post('/', function(req, res) {
 
     let order = req.body;
 
-    op.processOrder(order);
+    if (! (req.is("application/json") && validateOrder(order)))
+        res.status(400).end();
 
-    res.status(201).end();
+    else {
+        op.processOrder(order);
+        res.status(201).end();
+    }
 });
 
 
+function validateOrder(order) {
+
+    if( order.side != null &&
+        order.symbol != null &&
+        order.quantity != null &&
+        order.type != null &&
+        order.duration != null &&
+        order.account != null &&
+        order.broker != null &&
+        order.portfolio_id != null ) {
+
+        if (order.type == 'LMT' && order.price == null)
+            return false;
+
+        if (! checkDb(order.portfolio_id, order.account, order.broker))
+            return false;
+
+        return true;
+    }
+        
+    return false;
+}
+
+
+// check whether the provided information is consistent with existing db entries
+
+function checkDb(portfolio, account, broker) {
+
+    return true;
+}
 
 
 
