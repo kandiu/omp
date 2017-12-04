ad932e769148f304cff9056d7e978622f9b8cc17
robix82 <robertjans82@outlook.com>
2017-12-04 10:41:08 +0100

    added assets (underlyings or instruments) to userData

--- routes/login/login.js
+++ routes/login/login.js
@@ -6,6 +6,10 @@ const User = models.User;
 const Portfolio = models.Portfolio;
 const Account = models.Account;
 const AssetClass = models.AssetClass;
+const Equity = models.Equity;
+const Index = models.Index;
+const Future = models.Future;
+const Option = models.Option;
 
 router.get('/', function(req, res){
 	res.status(200).json({});
@@ -132,11 +136,105 @@ function sendUserData(userObject, res) {
 
         userData.brokers = mappings;
 
-        finish();
+        addAssets(0, 0);
+    }
+
+    function addAssets(portfolioIdx, assetListIdx) {
+
+        if (portfolioIdx == userData.portfolios.length) {
+            finish();
+        }
+
+        if (userData.assets == undefined) 
+            userData.assets = [];
+
+        let pf = userData.portfolios[portfolioIdx];
+
+        if (pf != undefined &&
+            pf.settings != undefined &&
+            pf.settings.selected_assets != undefined ) {
+
+            if (assetListIdx == pf.settings.selected_assets.length) {
+                addAssets(++portfolioIdx, 0);
+            }
+            else {
+
+                let mapping = { portfolio_id : pf.symbol, 
+                                asset_selection : assetListIdx,
+                                asset_class : pf.settings.selected_assets[assetListIdx].asset_class,
+                                assets : [] }
+
+                let assetNames = [];
+
+                if (pf.settings.selected_assets[assetListIdx].instruments.length > 0)
+                    assetNames = pf.settings.selected_assets[assetListIdx].instruments;
+                
+                if (pf.settings.selected_assets[assetListIdx].underlyings.length > 0)
+                    assetNames = pf.settings.selected_assets[assetListIdx].underlyings;
+
+
+                if (mapping.asset_class === 'Equity') {
+
+                    console.log("looking for equities");
+
+                    Equity.find({ "instrument_id" : { $in : assetNames} },
+
+                        function(err, found) {
+
+                            if (err) throw err;
+
+                            mapping.assets = found;
+                            userData.assets.push(mapping);
+                            addAssets(portfolioIdx, ++assetListIdx);
+                        });
+                }
+
+                if (mapping.asset_class === 'Index') {
+
+                    Index.find({ "instrument_id" : { $in : assetNames} },
+
+                        function(err, found) {
+
+                            if (err) throw err;
+
+                            mapping.assets = found;
+                            userData.assets.push(mapping);
+                            addAssets(portfolioIdx, ++assetListIdx);
+                        });
+                }
+
+                if (mapping.asset_class === 'Future') {
+
+                    Future.find({ "instrument_id" : { $in : assetNames} },
+
+                        function(err, found) {
+
+                            if (err) throw err;
+
+                            mapping.assets = found;
+                            userData.assets.push(mapping);
+                            addAssets(portfolioIdx, ++assetListIdx);
+                        });
+                }
+
+                if (mapping.asset_class === 'Option') {
+
+                    Option.find({ "instrument_id" : { $in : assetNames} },
+
+                        function(err, found) {
+
+                            if (err) throw err;
+
+                            mapping.assets = found;
+                            userData.assets.push(mapping);
+                            addAssets(portfolioIdx, ++assetListIdx);
+                        });
+                }             
+            }
+        }
     }
 
     function finish() {
-  //      res.set("Content-Type", "application/json");
         res.status(200).json(userData);
     }
 }
