bd685710421b52d5219c3ae6fd1f79f058412bab
rebigor <rebigor@gmail.com>
2017-12-05 15:39:48 +0100

    nothing special

---
+++ fix/test.js
@@ -0,0 +1,17 @@
+var fixclient = require("./orderProcessing.js");
+fixclient.connect()
+
+
+//Example of NewSingleOrder
+
+order = {
+	 type: "SingleOrder",
+     account: "Acount123",
+     side: "1",
+     clOrdID: "asf1235axaf",
+     symbol: "EBAY",
+     ordType: "1"
+}
+       
+fixclient.send(order);
+
