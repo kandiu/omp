d40ae13b2f3585d6bd75969c2234a0937778865d
EljonHarlicaj <harlie@usi.ch>
2017-12-05 18:34:17 +0100

    Added routes for analytics

--- routes/book/book.js
+++ routes/book/book.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const models = require('../../models');
 const Book = models.Book;
-
-
 // GET /
 
 router.get('/', function(req, res) {
@@ -21,19 +19,55 @@ router.get('/', function(req, res) {
 
 
 
-router.get('/op', function(req, res) {
-
-    Book.aggregate([{$group: {_id: "$security_symbol", total: {$sum: "$quantity"}}},
-        {$sort: {total: -1}}]
+router.get('/openpositions', function(req, res) {
+    Book.aggregate([{$group: {_id: "$security_symbol", total: {$sum: "$quantity"}}}
+        ]
         , function(err, found) {
-
-        if (err || found == null)
+        let toRet = [];
+        for(let i=0; i<found.length; i++){
+            if (found[i].total != "0"){
+                //found[i].
+                toRet.push(found[i])
+            }
+        }
+        console.log(toRet);
+        if (err || toRet == null)
             res.status(404).end();
         else {
-            res.status(200).json(found);
+            res.status(200).json(toRet);
         }
     });
 });
+
+
+router.get('/openpricepos/:_id', function(req, res) {
+    console.log("req.params._id", req.params._id);
+    Book.find({ security_symbol: req.params._id}
+        ,function(err, found) {
+            if (err || found == null)
+                res.status(404).end();
+            else {
+            let toret =found.sort({timestamp : 1, price: 1});
+                res.status(200).json(toret);
+                console.log("found: ",found);
+            }
+        });
+});
+
+router.get('/openpriceneg/:_id', function(req, res) {
+    console.log("req.params._id", req.params._id);
+    Book.find({ security_symbol: req.params._id}
+        ,function(err, found) {
+            if (err || found == null)
+                res.status(404).end();
+            else {
+                let toret = found.sort({timestamp : 1, price: 1});
+                res.status(200).json(toret);
+                console.log("found: ",found);
+            }
+        });
+});
+
 
 
 
