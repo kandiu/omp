ff353e27b943c1787e2db4e7994b50214763748d
Marcel Bezdrighin <bezdrm@usi.ch>
2017-12-06 18:56:09 +0100

    some more fixes to initiator

--- fix/startfix.sh
+++ fix/startfix.sh
@@ -10,7 +10,7 @@ sleep 3
 #start quickfix_inititiator
 (cd $DIR/quickfix_initiator/bin && java -cp quickfixj.jar:gson-2.8.2.jar:mina-core-2.0.16.jar:gso2.8.2.jar:slf4j-api-1.7.25.jar:slf4j-jdk14-1.7.25.jar:. quickfix_initiator/OMPInitiator) &
 
-sleep 3
+sleep 5
 #start server
 (cd $DIR/../ && DEBUG='*' nodemon ./bin/www) & 
 
